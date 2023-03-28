16bb8edfdc6b4f650aee1585bf63726b15a8a573
Nikita Tsygankov <like_fender@mail.ru>
2023-03-29 00:47:06 +0300

    add task 4 corrrect

--- TASK_4/Program.cs
+++ TASK_4/Program.cs
@@ -1,4 +1,11 @@
 ﻿// Напишите программу, которая на вход принимает число (N), 
 // а на выходе показывает все чётные числа от 1 до N
 
-Console.WriteLine("");
+Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i <= n; i++)
+{
+    if (i % 2 == 0)
+    System.Console.Write(i + ", ");
+}
