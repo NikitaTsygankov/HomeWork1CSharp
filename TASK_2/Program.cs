199d9c415cf57d96da049020a1084e5bcf9658b3
Nikita Tsygankov <like_fender@mail.ru>
2023-03-28 23:12:29 +0300

    add code task 2

--- TASK_2/Program.cs
+++ TASK_2/Program.cs
@@ -6,17 +6,12 @@ int a = int.Parse(Console.ReadLine());
 Console.WriteLine("ВВЕДИТЕ ВТОРОЕ ЧИСЛО:");
 int b = int.Parse(Console.ReadLine());
 
-if (a > b)
-{
-    Console.WriteLine("большее число"); 
-    Console.WriteLine(a);
-    Console.WriteLine("меньшее число");
-    Console.WriteLine(b);
-}
-else 
-{
-    Console.WriteLine("большее число"); 
-    Console.WriteLine(b);
-    Console.WriteLine("меньшее число");
-    Console.WriteLine(a);  
-}
+Console.WriteLine("ВВЕДИТЕ ТРЕТЬЕ ЧИСЛО:");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (a < b) max = b;
+if (b < c) max = c;
+
+System.Console.Writeline("Максимальное из этих чисел = "+max);
