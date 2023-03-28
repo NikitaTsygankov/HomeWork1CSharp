197b72822134562704ca51ce72570e2885fabf74
Nikita Tsygankov <like_fender@mail.ru>
2023-03-28 21:13:38 +0300

    task 1 done added

--- TASK_1/Program.cs
+++ TASK_1/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿//Напишите программу, которая на вход принимает 2 числа и выдает, большее и меньшее
+
+Console.WriteLine("ВВЕДИТЕ ПЕРВОЕ ЧИСЛО:");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("ВВЕДИТЕ ВТОРОЕ ЧИСЛО:");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("большее число"); 
+    Console.WriteLine(a);
+    Console.WriteLine("меньшее число");
+    Console.WriteLine(b);
+}
+else 
+{
+    Console.WriteLine("большее число"); 
+    Console.WriteLine(b);
+    Console.WriteLine("меньшее число");
+    Console.WriteLine(a);  
+}
