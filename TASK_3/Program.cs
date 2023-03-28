9f24eb3dcd68c09acbbf336d031f22f55e7bc15b
Nikita Tsygankov <like_fender@mail.ru>
2023-03-29 00:04:17 +0300

    task 3 done

---
+++ TASK_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу, в которой при вводе определяется четность числа
+
+Console.WriteLine("Введите число");
+
+int number = int.Parse(Console.ReadLine());
+
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+    Console.WriteLine("Число не четное");
+}
